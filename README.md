6fd67be71701ba8dabbd7158a5ba5f1503366f66
LostDakota <drew.ubuntuserver@gmail.com>
2015-12-02 12:23:04 -0500

    Edits

--- README.md
+++ README.md
@@ -3,32 +3,43 @@
 This bash script will run a two pass re-encode on a video 
 file with the intent to change the file size to something 
 more storage friendly. 
-Supported files include, but are not limited to .mkv, .avi, and .mp4.
+Supported files include, but are not limited to .mkv, .avi,
+and .mp4.
 
 ##SUPPORT
 -------------------------------------------------------
 This script will run on a Debian/Ubuntu system. A temp 
-directory will be utilized to store the output files. 
-You can change where this is located by adjusting the $TEMPDIR 
-variable at the top of the script. To run the script, chmod it 
-to +x and supply a video file as the only argument. 
-You should use absolute path values, as an option is to 
-replace the original file, however you do not have to overwrite it.
+directory (~/transcode) will be utilized to store the
+output files. You can change where this is located by
+adjusting the $THEDIR variable at the top of the script.
+To run the script, chmod it to +x and supply a video 
+file as the first argument. You should use absolute path
+values, as an option is to replace the original file.
+You must supply a second argument after the file.
+This is the target file size in megabytes. To change
+the speed of the encoding, alter the $PRESET variable.
+I have found that the slower you can tolerate, the better
+the encoding. By default, this is set to 'veryslow', and
+it is... very slow.
+
 The linux utility 'screen' is also helpful, due to the 
 long duration of encoding video.
+
 ```sh
 screen -dmS SOME_TITLE
 ```
 And then run:
 ```sh
 screen -r SOME_TITLE
-./mkvShrink.sh ~/Movies/Title\ of\ the\ Movie.mkv
+./mkvShrink.sh ~/Movies/"Title of the Movie.mkv" 700
 ```
 
 
 ##DEPENDENCIES
 -------------------------------------------------------
-You will need avconv and mediainfo, both available to you in the Ubuntu repositories.
+You will need avconv and mediainfo, both available to you in
+the Ubuntu repositories.
+
 ```sh
 sudo apt-get install avconv mediainfo
 ```
--- mkvShrink.sh
+++ mkvShrink.sh
@@ -1,87 +1,61 @@
 #!/bin/bash
 
-MOVIE=$1
 DURATION=`mediainfo --Inform="General;%Duration%" "$1"`
-SECONDS=`expr $DURATION / 1000`
-MINUTES=`expr $DURATION / 60000`
-BASENAME=$(basename "$MOVIE")
-NEWLINE=`printf '\n'`
+INSECONDS=$(($DURATION / 1000))
+EXTRA=`printf "%.0f" $(echo "scale=2;$2*.05" | bc)`
+FULL=$(($2 + $EXTRA))
+SIZE=$(($FULL * 1000 * 8))
+BITRATE=$(($SIZE / $INSECONDS - 128))
+BASENAME=$(basename "$1")
 FORMAT=`echo $BASENAME | cut -f2 -d"."`
-TEMPDIR=~/temp
+ORIGINALSIZE=`du "$1" | awk '{print $1}'`
+OSIZE=$(($ORIGINALSIZE / 1024))
+ACCEPTABLE=$(($2 + 5))
+BUFFER=$(($BITRATE / 2))
+THEDIR=~/transcode #Change this to wherever you like.
+PRESET="veryslow" #Your options are ultrafast, superfast, veryfast, faster, fast, medium, slow, slower and veryslow. Each is half as fast as the one before.
 
-clear
-
-if [ $FORMAT == mkv ] ; then
-	FORMAT=matroska
-elif [ $FORMAT == avi ] ; then
-	FORMAT=avi
-elif [ $FORMAT == mp4 ] ; then
-	FORMAT=mp4
+if [ $OSIZE -lt $ACCEPTABLE ] ;then
+	echo "Original size of $1 is sufficient at $OSIZE"
+	echo "Original size of $1 is sufficient at $OSIZE" >> ~/transcode/mkvShrink.log
 else
-	echo "I'm sorry. That filetype is not supported"
-fi
 
-echo -e "\e[94mWhat type of video is this? (film or animation)\e[0m"
-read TYPE
+	echo "Checking for working directory..."
 
-echo -e "\e[94mThe video is $SECONDS seconds long. ($MINUTES min.)"
-echo -e "What would you like the output size to be (in MB)?\e[0m"
-$NEWLINE
-read SIZE
+	if [ ! -d $THEDIR ] ; then
+		mkdir $THEDIR
+	fi
 
-SIZE=$(($SIZE * 1000 * 8))
-OVERHEAD=`echo "$SIZE * .02" | bc | cut -f1 -d'.'`
-SIZE=$(($SIZE + $OVERHEAD))
-BITRATE=$(($SIZE / $SECONDS - 128))
+	echo "Moving to working directory..."
 
-$NEWLINE
-echo -e "\e[94mThe bitrate will be $BITRATE"
-echo "We will save the converted file to $TEMPDIR/$BASENAME"
-echo "------------------------------------------------"
+	cd $THEDIR
 
-if [ ! -d ~/temp ] ; then
-	mkdir $TEMPDIR
-	echo "Creating the temp directory at $TEMPDIR"
-fi
+	echo "The video bitrate will be: $BITRATE"
 
-cd ~/temp
+	sleep 2
 
-$NEWLINE
-echo -e "\e[31mContinue? Y or N\e[0m"
+	START=`date +%s`
 
-read CONTINUE
+	avconv -y -i "$1" -pass 1 -c:v libx264 -b:v $BITRATE\k -preset veryslow -threads auto -an -f null -
 
-if [ $CONTINUE == Y ] ; then
-	$NEWLINE
-	echo "Here we go..."
-	sleep 1
-	START1=`date +%s`
-	$NEWLINE
-	avconv -y -i "$MOVIE" -pass 1 -c:v libx264 -b:v $BITRATE\k -c:a libmp3lame -b:a 128k -tune $TYPE -preset slow -threads 1 -f $FORMAT -sn /dev/null & plexTest.sh
-	PASS1=$((`date +%s` - $START1))
-	START2=`date +%s`
-	avconv -i "$MOVIE" -pass 2 -c:v libx264 -b:v $BITRATE\k -c:a libmp3lame -b:a 128k -tune $TYPE -preset slow -threads 1 -sn "$BASENAME" & plexTest.sh
-	PASS2=$((`date +%s` - $START2))
-	BSIZE=`stat -c %s "$BASENAME"`
-	GSIZE=`echo "scale=1;$BSIZE / 1000000 / 1024" | bc -l`
-	echo -e "\e[94mPass 1 completed in $(($PASS1 / 60)) minutes."
-	echo -e "\e[94mPass 2 completed in $(($PASS2 / 60)) minutes."
-	echo -e "\e[32mThe resulting file is $GSIZE G.\e[0m"
-	echo -e "\e[31mDo you want to replace the old with the new? Y or N\e[0m"
-#Here you can use twidge to be notified when your file is done.
-#	twidge update "$BASENAME is ready to be processed. `date +%l:%M%P`"
+	avconv -i "$1" -pass 2 -c:v libx264 -b:v $BITRATE\k -c:a libmp3lame -b:a 128k -preset veryslow -threads auto -sn "$BASENAME"
+
+	END=`date +%s`
+
+	TOTAL=$(($END - $START))
+
+	echo "Encoding of $BASENAME completed in `expr $TOTAL / 60` minutes"
+
+	echo "Encoding of $BASENAME completed in `expr $TOTAL / 60` minutes at `date +%l:%m' '%a' '%D`" >> ~/transcode/mkvShrink.log
+
+	echo "Would you like to replace the original? (Y or N)"
 
 	read REPLACE
-	if [ $REPLACE == Y ] ; then
-		echo -e "\e[32mMoving $BASENAME to its final resting place...\e[0m"
-		mv -v "$BASENAME" "$1"
-		echo -e "\e[32mRemoving leftover avconv files...\e[0m"
-		rm -v av*
-		echo "Conversion and replacement complete."
+
+	if [ $REPLACE = Y ] ; then
+		mv $BASENAME $1
 	else
-		echo "Files are retained here."
+		echo "Output file is retained in `pwd`";
 	fi
-else
-	$NEWLINE
-	echo -e "\e[31m Script aborted."
+
 fi
